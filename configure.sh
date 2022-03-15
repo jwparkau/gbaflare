f60a60a97cfbe4e14f49855acb3d8ab12e844de3
Jinwoo Park <jinwoo@jinwoopark.id.au>
2022-03-15 23:01:58 +1100

    simplify building

--- configure.sh
+++ configure.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-mkdir -p build && cd build
-
-if [ -z "$1" ]; then
-	platform="QT5"
-elif [ "$1" = "QT5" ]; then
-	platform="QT5"
-else
-	echo "supported platforms"
-	echo "QT5"
-	exit 1
-fi
 #-DCMAKE_CXX_FLAGS=-pg -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release -DUSE_"$platform"=1 ..
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Release -Bbuild
